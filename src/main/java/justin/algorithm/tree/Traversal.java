b062e1f1a748f279f520b80f6b076d3e5056db19
justin.luo <bayern01kahn@gmail.com>
2020-03-23 17:09:39 +0800

    加入层序遍历

--- src/main/java/justin/algorithm/tree/Traversal.java
+++ src/main/java/justin/algorithm/tree/Traversal.java
@@ -2,16 +2,23 @@ package justin.algorithm.tree;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 
 /**
  * 递归实现:
  * 创建二叉树
+ *
+ * 深度优先
  * 前序遍历(根->左->右)
  * 中序遍历(左->根->右)
  * 后序遍历(左->右->根)
  *
  *  即  根节点 在前就是 前序,  在中间 就是 中序,  在后面 就是 后序
+ *
+ *  广度优先
+ *  层序遍历 (利用队列来实现)
+ *
  */
 public class Traversal {
 
@@ -39,6 +46,9 @@ public class Traversal {
         midOrderTraversal(treeNode);
         System.out.println("后序遍历:");
         postOrderTraversal(treeNode);
+
+        System.out.println("层序遍历:");
+        levelOrderTraversal(treeNode);
         
     }
 
@@ -77,6 +87,21 @@ public class Traversal {
         postOrderTraversal(treeNode.leftChild);
         postOrderTraversal(treeNode.rightChild);
         System.out.println(treeNode.data);
+    }
+
+    private static void levelOrderTraversal(TreeNode treeNode){
+        Queue<TreeNode> queue = new LinkedList();
+        queue.offer(treeNode);
+        while(!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            System.out.println(node.data);
+            if (node.leftChild != null){
+                queue.offer(node.leftChild);
+            }
+            if (node.rightChild != null){
+                queue.offer(node.rightChild);
+            }
+        }
     }
     
 }
