4e87da57bebe40481bd466d273cf8f42ac4c1def
justin.luo <bayern01kahn@gmail.com>
2020-03-26 16:34:40 +0800

    枚举单例 测试 正常使用 和 反射无法破解证明

--- src/main/java/justin/designPattern/singleton/enumSingleton_recommend/EnumSingletonTest.java
+++ src/main/java/justin/designPattern/singleton/enumSingleton_recommend/EnumSingletonTest.java
@@ -5,14 +5,26 @@ import java.lang.reflect.Constructor;
 public class EnumSingletonTest {
 
     public static void main(String[] args) {
+
+        //正常使用
+         Object obj = new Object();
+         Object obj1 = new Object();
+
+         EnumSingleton e = EnumSingleton.getInstance();
+         e.setData(obj);
+         System.out.println(e.getData().toString());
+         e = EnumSingleton.getInstance();
+         e.setData(obj1);
+         System.out.println(e.getData().toString());
+
+        // 演示 枚举单例无法被反射暴力破解
         try {
             Class clazz = EnumSingleton.class;
             Constructor c = clazz.getDeclaredConstructor(String.class,int.class);
             c.setAccessible(true);
             EnumSingleton enumSingleton = (EnumSingleton)c.newInstance("Tom",666);
-
-        }catch (Exception e){
-            e.printStackTrace();
+        }catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 
